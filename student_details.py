81376778713435099b08147a7782504f33f4594d
Saboor SIddiqui <saboor@Saboors-MacBook-Air.local>
2024-10-07 14:40:43 +0530

    Changes for student-detaila page

--- student_details.py
+++ student_details.py
@@ -4,15 +4,21 @@ import os
 import base64
 
 def student_details_by_id(student_id):
-    student = get_student_by_id_from_db(student_id)  # Fetch student from the database
+    student = get_student_by_id_from_db(student_id)
     if student:
         print(f"Fetching details : {student}")
-        photo_path = os.path.join('static', 'photos', student['photo'].decode('utf-8'))  # Decode the bytes object to a string
-        if os.path.exists(photo_path):
-            with open(photo_path, 'rb') as photo_file:
-                student['photo'] = base64.b64encode(photo_file.read()).decode('utf-8')
+        photo_filename = student['photo'].decode('utf-8')
+        if photo_filename:  # Ensure the filename is not empty
+            photo_path = os.path.join('static', 'photos', photo_filename)
+            if os.path.isfile(photo_path):  # Check if it's a file
+                with open(photo_path, 'rb') as photo_file:
+                    student['photo'] = base64.b64encode(photo_file.read()).decode('utf-8')
+            else:
+                print(f"Photo file does not exist: {photo_path}")
+        else:
+            print("No photo filename provided.")
         return student
     else:
         return "Student not found", 404
 
-    
+
